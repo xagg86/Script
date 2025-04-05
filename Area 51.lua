89ca12db1638fbd01f291c2cce11cfc63f925b74
xagg86 <ludatarasuk6@gmail.com>
2025-04-05 19:23:32 +0300

    Survive in Area 51.lua

---
+++ Area 51.lua
@@ -0,0 +1,25 @@
+repeat task.wait() until game:IsLoaded()
+local Wizard = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+local Window = Wizard:NewWindow("Script Hub")
+local Section = Window:NewSection("Hidden Weapons")
+Section:CreateButton("Ice Crossbow", function()
+game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(
+end)
+Section:CreateButton("LDX", function()
+game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(
+end)
+Section:CreateButton("First Person", function()
+game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(
+end)
+Section:CreateButton("Unlimited Zoom", function()
+game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(
+end)
+Section:CreateButton("Touch Interest", function()
+game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(
+end)
+Section:CreateButton("Enter a HitBox", function()
+game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(
+end)
+Section:CreateButton("Enter a HitBox", function()
+game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(
+end)
